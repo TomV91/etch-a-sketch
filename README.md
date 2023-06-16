8f88f0f7234ff8aba2cd8536bb0f41be95395d69
Foxtom91 <tommaso.varvello@gmail.com>
2023-06-16 21:16:16 +0100

    Add files (coded offline)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# etch-a-sketch
+# Etch-A-Sketch
+
+An online sketch pad as part of the curriculum of The Odin Project. 
---
+++ scripts.js
@@ -0,0 +1,129 @@
+// SET UP GIT BEFORE CONTINUING!
+
+// TO DOS: Add Grayscale  - Add label indicating canvas size (dynamically update with JS)
+// ADD CSS TO STYLE PAGE TO LOOK NICE
+
+// Initializing variables
+
+let container = document.querySelector('#pad-container');
+let mouse_drag = false;
+let canvas_size = 16;
+let square;
+
+// Window mouse events
+
+window.addEventListener('mousedown', (event) => {
+    mouse_drag = true;
+})
+
+window.addEventListener('mouseup', (event) => {
+    mouse_drag = false;
+} )
+
+// Helper Functions
+
+function create_pad(size) {
+    for (let i = 0; i < Math.pow(size,2); i++) {
+        let create_square = document.createElement("div");
+        create_square.id = `square${i}`;
+        create_square.classList.add('canvas-square');
+        create_square.addEventListener('click', click_black);
+        create_square.addEventListener('mouseenter', drag_black);
+        container.appendChild(create_square);
+    }
+}
+
+function delete_pad() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function custom_pad() {
+    let custom_canvas = Number(prompt('Please enter the amount of squares per side (maximum 100)', '16'));
+    if (custom_canvas === NaN || custom_canvas === 0 || custom_canvas > 100) {
+        alert('Please enter a valid number between 1 and 100!');
+        custom_pad();
+    } else {
+        delete_pad();
+        canvas_size = custom_canvas;
+        document.querySelector(':root').style.setProperty('--square_px', `${600/canvas_size}px`)
+        create_pad(canvas_size);
+    }
+}
+
+function set_black() {
+    for (let i = 0; i < Math.pow(canvas_size,2); i++) {
+        square = document.querySelector(`#square${i}`);
+        square.removeEventListener('click', click_rainbow);
+        square.removeEventListener('mouseenter', drag_rainbow);
+        square.removeEventListener('click', click_eraser);
+        square.removeEventListener('mouseenter', drag_eraser);
+        square.addEventListener('click', click_black);
+        square.addEventListener('mouseenter', drag_black);
+    }
+}
+
+function set_rainbow() {
+    for (let i = 0; i < Math.pow(canvas_size,2); i++) {
+        square = document.querySelector(`#square${i}`);
+        square.removeEventListener('click', click_black);
+        square.removeEventListener('mouseenter', drag_black);
+        square.removeEventListener('click', click_eraser);
+        square.removeEventListener('mouseenter', drag_eraser);
+        square.addEventListener('click', click_rainbow);
+        square.addEventListener('mouseenter', drag_rainbow);
+    }
+}
+
+function set_eraser() {
+    for (let i = 0; i < Math.pow(canvas_size,2); i++) {
+        square = document.querySelector(`#square${i}`);
+        square.removeEventListener('click', click_black);
+        square.removeEventListener('mouseenter', drag_black);
+        square.removeEventListener('click', click_rainbow);
+        square.removeEventListener('mouseenter', drag_rainbow);
+        square.addEventListener('click', click_eraser);
+        square.addEventListener('mouseenter', drag_eraser);
+    }
+}
+
+function click_black(event) {
+    event.target.style.backgroundColor = 'black';
+}
+
+function drag_black(event) {
+    if (mouse_drag === true) {
+        event.target.style.backgroundColor = 'black';
+    }
+}
+
+function click_rainbow(event) {
+    event.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function drag_rainbow(event) {
+    if (mouse_drag === true) {
+        event.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+}
+
+function click_eraser(event) {
+    event.target.style.backgroundColor = 'white';
+}
+
+function drag_eraser(event) {
+    if (mouse_drag === true) {
+        event.target.style.backgroundColor = 'white';
+    }
+}
+
+function clear_pad() {
+    for (let i = 0; i < Math.pow(canvas_size,2); i++) {
+        document.querySelector(`#square${i}`).style.backgroundColor = 'white';
+    }
+}
+
+// Builds default pad
+
+create_pad(canvas_size);
